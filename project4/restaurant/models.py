9c67170beebdd3595dc9fd23d99fa0dd3520fe2a
Perparim Shabani <perparimshabani.97@gmail.com>
2024-03-21 18:49:18 +0100

    models.py modified

--- project4/restaurant/models.py
+++ project4/restaurant/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.db.models.signals import post_migrate
+from django.dispatch import receiver
 
 class Restaurant(models.Model):
     total_seats=models.IntegerField(default=40)
@@ -8,3 +10,8 @@ class Reservations(models.Model):
     restaurant = models.ForeignKey(Restaurant, on_delete=models.CASCADE)
     guests = models.IntegerField()
     id = models.AutoField(primary_key=True)
+    
+@receiver(post_migrate)
+def create_restaurant(sender, **kwargs):
+    if sender.name == 'your_app_name' and Restaurant.objects.count() == 0:
+        Restaurant.objects.create(name="Restaurant")
